b881fabe856a4abf95563d6c287fa72a99c02179
Paweł <pawel.janik.1983@gmail.com>
2024-02-28 13:53:23 +0100

    modified ignore to omit the Vite data

---
+++ src/JS/ip_tracker.js
@@ -0,0 +1,11 @@
+// ipify endpoint
+// GET https://geo.ipify.org/api/v2/country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
+
+// possibl endpoint v2/country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
+
+// apiKey
+// Required. Get your personal API KEY on My subscriptions page.
+// ipAddress
+// Optional. IPv4 or IPv6 to search location by.
+
+// If the parameter is not specified, then it defaults to client request's public IP address.
--- src/JS/main.js
+++ src/JS/main.js
@@ -10,15 +10,3 @@ import "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
 import "../JS/leaflet.js";
 
 const API = new fetchWrapper("https://geo.ipify.org/api/");
-
-// ipify endpoint
-// GET https://geo.ipify.org/api/v2/country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
-
-// possibl endpoint v2/country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
-
-// apiKey
-// Required. Get your personal API KEY on My subscriptions page.
-// ipAddress
-// Optional. IPv4 or IPv6 to search location by.
-
-// If the parameter is not specified, then it defaults to client request's public IP address.
