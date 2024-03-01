ef7777dc8e8e79915bdd0377d3fdbd213f18532c
Paweł <pawel.janik.1983@gmail.com>
2024-03-01 01:13:52 +0100

    fixed CORS error by adding a proxy

--- JS/ip_tracker.js
+++ JS/ip_tracker.js
@@ -1,7 +1,7 @@
 // ipify endpoint
 // GET https://geo.ipify.org/api/v2/country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
 
-// possibl endpoint v2/country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
+// possibl endpoint /country?apiKey=at_njfhKTlrrwuKrd28LYnFyxLvHZ2uc&ipAddress=8.8.8.8
 
 // apiKey
 // Required. Get your personal API KEY on My subscriptions page.
@@ -9,3 +9,20 @@
 // Optional. IPv4 or IPv6 to search location by.
 
 // If the parameter is not specified, then it defaults to client request's public IP address.
+let ipAddress = '8.8.8.8'
+const api_key = import.meta.env.VITE_IPIFY_APIKey
+
+try {
+    fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${api_key}&ipAddress=${ipAddress}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            let lat = data.location.lat
+            let lng = data.location.lng
+            L.map('map').setView([lat, lng], 13)
+        })
+} catch (error) {
+    console.error(error)
+}
--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,10 @@
-// API wrapper class
-import fetchWrapper from "../JS/fetch-wrapper.js";
-// SCSS
-import "../SCSS/main.scss";
 // BOOTSTRAP
-import * as bootstrap from "bootstrap";
+import * as bootstrap from 'bootstrap'
+// SCSS
+import '/SCSS/main.scss'
 // LEAFLET map creator JS
-import "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
+import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 // LEEAFLET custom js
-import "../JS/leaflet.js";
-
-const API = new fetchWrapper("https://geo.ipify.org/api/");
+import '/JS/leaflet.js'
+// Ipify
+import '/JS/ip_tracker.js'
